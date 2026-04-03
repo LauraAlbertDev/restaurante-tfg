96985c3363f19ab2ebb2a3ae0885ed1b05e072cd
Daneeerya <daneeerya@gmail.com>
2026-04-03 14:03:56 +0200

    fix: Añadir validadores

--- frontend/src/app/Validators/FormValidators.ts
+++ frontend/src/app/Validators/FormValidators.ts
@@ -1,43 +1,46 @@
-import {FormControl, ValidationErrors} from '@angular/forms';
+import {AbstractControl, FormControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
 export class FormValidators {
 
   static nonOnlyWhiteSpace(control: FormControl): ValidationErrors | null {
-    if((control.value != null) && (control.value.trim().length == 0)) {
-      return {nonOnlyWhiteSpace: true};
-    }else {
-      return null;
-    }
-  }
-
-    static minToday(control: FormControl): ValidationErrors | null {
-    if (!control.value) return null;
-
-    const inputDate = new Date(control.value);
-    const today = new Date();
-
-    // quitar horas para comparar solo fecha
-    today.setHours(0,0,0,0);
-    inputDate.setHours(0,0,0,0);
-
-    if (inputDate < today) return { minToday: true };
-
-
-    return null;
+    if((control.value != null) && (control.value.trim().length == 0)) return {nonOnlyWhiteSpace: true};
+    else return null;
   }
 
   static emailDominio(dominio: string) {
     return (control: FormControl): ValidationErrors | null => {
       const email = control.value;
-
       if (!email) return null;
-
-      if (!email.toLowerCase().endsWith('@' + dominio.toLowerCase())) {
-        return { emailDominio: true };
-      }
-
+      if (!email.toLowerCase().endsWith('@' + dominio.toLowerCase())) return { emailDominio: true };
       return null;
     };
   }
 
+  static minValue(value: number): ValidationErrors | null{
+    return (control: FormControl): ValidationErrors | null => {
+      if (control.value < value) return {minValue: true};
+      else return null;
+    }
+  }
+
+  static allowedExtension(regex: RegExp): ValidationErrors | null{
+    return (control: FormControl): ValidationErrors | null => {
+      const allowed = regex.test(control.value);
+      return allowed? null: {allowedExtension: true};
+    }
+  }
+
+  static minDate(minDateStr: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) return null;
+
+      const selectedDate = new Date(control.value);
+      selectedDate.setHours(0, 0, 0, 0);
+
+      const minDate = new Date(minDateStr);
+      minDate.setHours(0, 0, 0, 0);
+
+      return selectedDate < minDate ? { minDateError: true } : null;
+    };
+  }
 }
